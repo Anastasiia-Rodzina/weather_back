258098b068efb68546eefd54d5dd10ad6723c711
Anastasiia-Rodzina <pototzkaja.n@gmail.com>
2024-11-20 16:30:20 +0200

    fix for deploy

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import cors from "cors";
 import routerAuth from "./routes/routesAuth.js";
 import routerCities from "./routes/routesCities.js";
 import dotenv from "dotenv";
-
+import fs from "fs";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 dotenv.config();
+
 const { DB_HOST } = process.env;
 const port = process.env.PORT || 4000;
 export const app = express();
@@ -22,22 +22,27 @@ app.use(express.static("public"));
 app.use("/auth", routerAuth);
 app.use("/cities", routerCities);
 
+const swaggerDocument = JSON.parse(fs.readFileSync("./swagger.json", "utf-8"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server is running!");
+});
+
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    console.log(`Starting server on port ${port}`);
+    console.log("MongoDB connected successfully");
     app.listen(port, () => {
-      console.log("Database connection successful");
+      console.log(`Server is running on port ${port}`);
     });
   })
   .catch((error) => {
-    console.log("MongoDB connection error:", error.message);
+    console.error("MongoDB connection error:", error.message);
     process.exit(1);
   });
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Route Not Found" });
